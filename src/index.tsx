e96cc0f01924c223bac98e65a773f242e2c75e0a
Satish Kumar <satish@ensuredit.com>
2023-05-28 23:20:23 +0530

    setup redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./AppRoutes/AppRoutes";
 import "./moke-api";
+import { Provider } from "react-redux";
+import store from "./store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <AppRoutes />
+    <Provider store={store}>
+      <AppRoutes />
+    </Provider>
+    ,
   </React.StrictMode>
 );
